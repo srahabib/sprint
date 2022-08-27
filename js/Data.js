623516b81b45b90ee6a0ae0304c36c09c3bda6dd
SaraHabib <sara.habib48@gmail.com>
2022-08-27 22:38:12 +0200

    initial commit

---
+++ js/Data.js
@@ -0,0 +1,58 @@
+let shopItemsData = [
+  {
+    id: "thyfhcbcv",
+    name: "Nvidia 1",
+    price: 300,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/rtx3060.jpg",
+  },
+  {
+    id: "iuertrywebncdjksadjk",
+    name: "Nvidia 2",
+    price: 700,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/rtx3060.jpg",
+  },
+  {
+    id: "thierytbvcbvzdhadjk",
+    name: "Nvidia 3",
+    price: 450,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/rtx3060.jpg",
+  },
+  {
+    id: "oiopijmjkhuihb",
+    name: "Nvidia 4",
+    price: 520,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/rtx3060.jpg",
+  },
+  {
+    id: "rtytytuyuytyytbvncv",
+    name: "Nvidia 5",
+    price: 350,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/rtx3060.jpg",
+  },
+  {
+    id: "lksakoenaihwneka",
+    name: "Nvidia 6",
+    price: 1600,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/rtx3060.jpg",
+  },
+  {
+    id: "yhjwokajdmoajsla",
+    name: "Nvidia 7",
+    price: 1200,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/rtx3060.jpg",
+  },
+  {
+    id: "zaasfgdlkaohwa",
+    name: "Nvidia 8",
+    price: 900,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/rtx3060.jpg",
+  }
+];
